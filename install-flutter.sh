c629def801fb56b16dc9144c7fb16eee78b471b7
hanif fajar <hanif@MacBook-Pro-3.local>
2025-01-21 00:04:55 +0700

    Menambahkan file netlify.toml dan konfigurasi build

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Install Flutter
-
-# Install dependencies first (in case they are missing)
-sudo apt-get update
-sudo apt-get install -y wget curl git unzip
+apt-get update
+apt-get install -y git curl unzip
 
 # Download and install Flutter
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
